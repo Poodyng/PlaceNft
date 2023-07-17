b1bb91ded901ef41cd80732f9a8c2c08560c8b07
Dima <prodan.dimon@gmail.com>
2023-07-17 15:57:00 +0300

    first commit

---
+++ assets/scripts/common.js
@@ -0,0 +1,70 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-2 .swiper-button-next',
+      prevEl: '.slider-2 .swiper-button-prev',
+    },
+  });
+
+  const swiper3 = new Swiper('.swiper3', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-3 .swiper-button-next',
+      prevEl: '.slider-3 .swiper-button-prev',
+    },
+  });
+
+
+  const swiper4 = new Swiper('.swiper4', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-4 .swiper-button-next',
+      prevEl: '.slider-4 .swiper-button-prev',
+    },
+  });
+
+  const swiper5 = new Swiper('.swiper5', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-5 .swiper-button-next',
+      prevEl: '.slider-5 .swiper-button-prev',
+    },
+  });
